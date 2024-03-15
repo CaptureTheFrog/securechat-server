6b85b10a328ef19ccd9c13d60e71da163ce3f32c
CaptureTheFrog <capturethefrog@proton.me>
2024-03-15 02:15:44 +0000

    Add grpc stubs

--- main.go
+++ main.go
@@ -1,10 +1,36 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	. "securechat-server/client_server"
+	pb "securechat-server/grpc"
 	. "securechat-server/server"
 )
 
+var (
+	port = flag.Int("port", 50051, "The server port")
+)
+
 func main() {
+	// to sort out merge code
+	// client_server branch
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	gs := grpc.NewServer()
+	pb.RegisterClientServerCommsServer(gs, &GRPCServer{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := gs.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+	// dht branch
 	s := NewServer("localhost:50051")
 
 	record := s.Get("test")
